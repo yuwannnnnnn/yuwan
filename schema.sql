dc23baf173d2a37cd0a41f18444df95923c9fe8b
yuwannnnnnn <159679489+yuwannnnnnn@users.noreply.github.com>
2024-08-31 16:04:08 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,28 +1,49 @@
-Select * 
-FROM Users
-WHERE DateOfBirth BETWEEN "01/01/2000" and "12/31/2004";
+-- Create Users Table
+CREATE TABLE Users (
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(50),
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    Password VARCHAR(255),
+    DateAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-Select * 
-FROM Posts
-WHERE PostedBy = 4;
-
-Select GroupName 
-FROM Groups;
-
-Select *
-FROM GroupMembershipRequests
-WHERE GroupMemberUserID = 2;
+-- Create Friends Table
+CREATE TABLE Friends (
+    FriendID INT AUTO_INCREMENT PRIMARY KEY,
+    FriendWhoAdded INT,
+    FriendBeingAdded INT,
+    IsAccepted BOOLEAN,
+    DateAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 
+-- Create Groups Table
+CREATE TABLE Groups (
+    GroupID INT AUTO_INCREMENT PRIMARY KEY,
+    GroupName VARCHAR(100),
+    CreatedBy INT,
+    DateAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-SELECT * 
-FROM FRIENDS
-WHERE FriendWhoAdded = 1;
 
-SELECT *
-FROM Posts
-WHERE GroupID = 2 and IsOnlyForFriends = 1;
+-- Create Posts Table
+CREATE TABLE Posts (
+    PostID INT AUTO_INCREMENT PRIMARY KEY,
+    PostDescription VARCHAR(255),
+    PostedBy INT,
+    IsPublic BOOLEAN,
+    IsOnlyForFriends BOOLEAN,
+    GroupID INT,
+    DatePosted TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-SELECT *
-FROM GroupMembershipRequests
-WHERE GroupID = 2 and IsGroupMemberShipAccepted = 0;
+-- Create Group Membership Requests Table
+CREATE TABLE GroupMembershipRequests (
+    GroupMemberShipRequestsID INT AUTO_INCREMENT PRIMARY KEY,
+    GroupID INT,
+    GroupMemberUserID INT,
+    IsGroupMemberShipAccepted BOOLEAN,
+    DateAccepted TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
